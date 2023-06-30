510a2127a6755f8cdb71f17f5eb692771fdcd401
706718242 <124694760+706718242@users.noreply.github.com>
2023-06-30 10:20:38 +0800

    Update tool1.js

--- tool1.js
+++ tool1.js
@@ -1,9 +1,12 @@
+var ln="c23b34";
+
 var c23a12 = ["1t3", "1t4", "2t3","2t4","3t3","3t4"];//23a-1-2
 var c23a34 = ["1t1", "1t2", "2t1","2t2","3t1","3t2","4t1"];//23a-3-4
 var c23c12 = ["1t3", "1t4", "2t3","2t4","3t3","3t4"];//23c-1-2
-var c23b34 = ["1t1", "1t2", "2t1", "2t2", "3t1", "2t5", "2t6"];//23b-3-4
+var c23b34 = ["1t1", "1t2", "2t1", "2t2", "3t1", "3t5", "2t5", "2t6"];//23b-3-4
 
 var searchText=[];
+var ip=[];
 
 //var http="172.30.141.220";//23a-3-4
 
@@ -23,10 +26,10 @@ var tsecond; //
 
 window.onload = (event) => {
  
-if(localStorage.getItem("Ln")=="c23a12"){searchText=c23a12;}
-if(localStorage.getItem("Ln")=="c23a34"){searchText=c23a34;}
-if(localStorage.getItem("Ln")=="c23c12"){searchText=c23c12;}
-if(localStorage.getItem("Ln")=="c23b34"){searchText=c23b34;}
+if(ln=="c23a12"){searchText=c23a12;ip="";}
+if(ln=="c23a34"){searchText=c23a34;ip="";}
+if(ln=="c23c12"){searchText=c23c12;ip="";}
+if(ln=="c23b34"){searchText=c23b34;ip="172.30.141.245"}
 
  
 //port.postMessage({type: 'saveData', data: {2: '321'}});
@@ -87,7 +90,7 @@ tid=setTimeout(function() {  keys=""; },1500);
   if(keys=="123"){  console.log(keys);}
  console.log(keys);
  //ctrl home
-if(event.keyCode==13&&keys=="open"){
+if(event.keyCode==13&&keys=="gl"){
 keys="";
 //if(localStorage.getItem("user")!=null){
 if(sessionStorage.getItem("flag")!="1"){
@@ -121,7 +124,7 @@ console.log(date);
  }
 
  
-if(event.keyCode==13&&keys=="pc"){
+if(event.keyCode==13&&keys=="js"){
 keys="";
 cr();
 
@@ -143,7 +146,7 @@ if(i!=null&&i!=""&&i!=" "){
         sendResponse({ status: request.status });
     }
 
-    request.open('POST', "http://"+localStorage.getItem("IP")+":9100/pstprnt", true);
+    request.open('POST', "http://"+ip+":9100/pstprnt", true);
     request.send("^XA ^MD //深度 ^PR //速度 ^BY1 //模块 ^FO302,12 //xy \
                  ^A0N,16,16 //Atn,x,y t条码字体 0-9 A-Z种字体 n方向 xy长宽^BCN,20,N,N,N  ^FD>:"
                  +i+"^FS  //条码 ~TA005 //撕纸位置 ^XZ");
@@ -164,7 +167,7 @@ if(i!=null&&i!=""&&i!=" "){
         sendResponse({ status: request.status });
     }
 
-    request.open('POST', "http://"+localStorage.getItem("IP")+":9100/pstprnt", true);
+    request.open('POST', "http://"+ip+":9100/pstprnt", true);
 
       request.send("^XA ^MD //深度 ^PR //速度 ^BY1 //模块 ^FO302,12 //xy \
                  ^A0N,20,20 //Atn,x,y t条码字体 0-9 A-Z种字体 n方向 xy长宽  ^FD"
@@ -189,7 +192,7 @@ if(i!=null&&i!=""&&i!=" "){
         sendResponse({ status: request.status });
     }
 
-    request.open('POST', "http://"+localStorage.getItem("IP")+":9100/pstprnt", true);
+    request.open('POST', "http://"+ip+":9100/pstprnt", true);
     request.send("^XA ^MD //深度 ^PR //速度 ^BY1 //模块 ^FO302,12 //xy \
                  ^A0N,16,16 //Atn,x,y t条码字体 0-9 A-Z种字体 n方向 xy长宽^BCN,20,Y,N,N  ^FD>:"
                  +i+"^FS  //条码 ~TA005 //撕纸位置 ^XZ");
@@ -252,20 +255,31 @@ var blc;
    //i++;
   // blc=parseInt(cells[1].getElementsByTagName("td")[2].innerText); 
   // console.log(blc);
-    
-   cells[i].style.backgroundColor = "red";
+     var row = table.insertRow(0);
+
+    row.innerHTML = cells[i+1].innerHTML;
+    row.style.backgroundColor = "red"; 
+    table.deleteRow(i+1);
+    i--;
+  // cells[i].style.backgroundColor = "red";
+   
    //i[i]=cells[i].innnrHTML;
    //cells[i].getElementsByTagName("td")[4].style="color:#337ab7";  //337ab7        
   }else{ 
    cells[i].style.backgroundColor = "#ffc107"; 
-        di[ii]=cells[i].innerHTML;ii++;
-        //table.deleteRow(i);
+       // di[ii]=cells[i].innerHTML;ii++;
+       
         //var row = table.insertRow(-1).innerHTML = cells[i].innerHTML;
+        //table.deleteRow(i);
+        //i--;
         //j[i]=cells[i].innnrHTML;
        }
   
  }
 
+
+ 
+  
  // for(var i =0 ;i<ii; i++) {
 
  
@@ -309,7 +323,7 @@ function cff( cfff) {
    b=bb;
    c=b/a;
    console.log(a+'/'+b+'='+c);
-
+  if(cells[i].getElementsByTagName("td")[6].innerText=="OnlineToBeMapped"){c=1;}
   //parseInt();
   }
 
@@ -341,7 +355,7 @@ var cells = table.getElementsByTagName("tr");
       //  sendResponse({ status: request.status });
    // }
 
-    request.open('POST', "http://"+http+":9100/pstprnt", true);
+    request.open('POST', "http://"+ip+":9100/pstprnt", true);
     request.send("^XA ^MD //深度 ^PR //速度 ^BY1 //模块 ^FO302,12 //xy \
                  ^A0N,16,16 //Atn,x,y t条码字体 0-9 A-Z种字体 n方向 xy长宽 ^BCN,20,Y,N,N ^FD>:"
                  +cells[i].getElementsByTagName("td")[1].innerText+"^FS  //条码 ~TA005 //撕纸位置 ^XZ");
@@ -356,33 +370,113 @@ var cells = table.getElementsByTagName("tr");
 function cr()
 {
 var table = document.getElementById("OnMachine");
-var cells = table.getElementsByTagName("tr");                                       
+var cells = table.getElementsByTagName("tr");    
+var table1 = document.getElementById("OnLine");
+var cells1 = table1.getElementsByTagName("tr"); 
 var max = localStorage.getItem("cr");           
-var use = 0;          
-var index = 0;                              // 记录最终结果所在的行号  
+//var use = 0;          
+var index = 0;
+var bl=[];
+var bli=0;
+var ok=0;
 
-for (var i = 1; i < cells.length; i++) {     
+
+ for(var j=0;j<searchText.length;j++){
+ for (var i = 1; i < cells.length; i++) {
+ var Text = cells[i].getElementsByTagName("td")[0].innerText.toLowerCase(); // 将单元格文本转换为小写字母
+ if (Text.indexOf(searchText[j]) != -1) {
+  
+  document.getElementById("OnMachine").deleteRow(i);
+   i--;
+   
+  }
+
+ }
+ }
+
+ 
+for (var i = 1; i < cells.length; i++){     
 var col0 = Number(cells[i].getElementsByTagName("td")[3].textContent);   
 var col1 = Number(cells[i].getElementsByTagName("td")[4].textContent);   
 var col2 = Number(cells[i].getElementsByTagName("td")[5].textContent);   
- use = col0-col1;
-if ((col1/col2 >= max)&&use!=0){                                                   
+//use = col0-col1;
+if ((col1/col2 >= max)&&ok==0){                                                   
   // max = col1;         
    index = i;  
  
-   break;                                                      
-  } 
+   ok=1;                                                  
+  }
+if(i<cells.length-1&&(cells[i].getElementsByTagName("td")[0].innerText)==(cells[i+1].getElementsByTagName("td")[0].innerText)){
+
+ var num1=Number(cells[i].getElementsByTagName("td")[3].innerText);  
+ var num2=Number(cells[i+1].getElementsByTagName("td")[3].innerText);
+ cells[i+1].getElementsByTagName("td")[3].innerText=num1+num2;
+ var num1=Number(cells[i].getElementsByTagName("td")[4].innerText);  
+ var num2=Number(cells[i+1].getElementsByTagName("td")[4].innerText);
+ cells[i+1].getElementsByTagName("td")[4].innerText=num1+num2;
+ document.getElementById("OnMachine").deleteRow(i);i--;
+}
 } 
 
-console.log("最大数量：" + cells[index].innerText);
- 
-var r1 = Number(cells[index].getElementsByTagName("td")[3].textContent);  
+
+
+
+//console.log("最大数量：" + cells[index].innerText);
+if(index==0){index=1;}
+var r1 = Number(cells[index].getElementsByTagName("td")[3].innerText);  
 var r2 = Number(cells[index].getElementsByTagName("td")[4].textContent);  
 var r3 = Number(cells[index].getElementsByTagName("td")[5].textContent);  
 // console.log("已完成：" +(r1-r2));
 var r4=(r1-r2)/r3;
- //alert("总数："+localStorage.getItem("cr") +"\n已完成：" +r4+"\n剩余：" +(localStorage.getItem("cr")-r4));
-  var crr=prompt("总数："+max +"\n已完成：" +r4+"\n剩余：" +(max-r4)+"\n输入总数");
-   if(crr!=null&&crr!=""){
-localStorage.setItem("cr",crr);}
+ 
+
+ dr();
+for (var i = 1; i < cells.length; i++){     
+ 
+var col1 = Number(cells[i].getElementsByTagName("td")[4].textContent);   
+var col2 = Number(cells[i].getElementsByTagName("td")[5].textContent);   
+
+ if (((col1/col2) < (max-r4))&&col1<2500){                                                   
+
+//bl[bli]=cells[i].getElementsByTagName("td")[0].innerText+"\n";
+//bli++;
+  
+var row=table1.insertRow(-1);
+   row.innerHTML = cells[i].innerHTML;
+   row.deleteCell(6);
+   row.deleteCell(3);
+   row.deleteCell(2);
+   row.insertCell(4)
+   var a = Number(row.getElementsByTagName("td")[2].textContent);   
+   var b = Number(row.getElementsByTagName("td")[3].textContent);  
+   row.getElementsByTagName("td")[4].innerText=parseInt(a/b);
+
+  
+  }
+}
+ 
+ var crr=prompt("总数："+max +"\n已完成：" +r4+"\n剩余：" +(max-r4)+"\n输入总数");
+
+
+ if(crr!=null&&crr!=""){
+localStorage.setItem("cr",crr);
+ }
+ co(); 
+ cf();
+ 
+}
+
+
+
+function dr()
+{  
+var table = document.getElementById("OnLine");
+var cells = table.getElementsByTagName("tr"); 
+
+ for (var i = 1; i < cells.length; i++) {
+  
+  document.getElementById("OnLine").deleteRow(i);
+ 
+   
+  }
 }
